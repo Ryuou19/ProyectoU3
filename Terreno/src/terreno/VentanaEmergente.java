3b43f12b0dce2911be22e4f57e303632035df1cc
Ryuou19 <bganora21@alumnos.utalca.cl>
2023-12-11 12:08:46 -0300

    Comentarios Ventana Emergente

--- Terreno/src/terreno/VentanaEmergente.java
+++ Terreno/src/terreno/VentanaEmergente.java
@@ -1,38 +1,29 @@
 package terreno;
-import javafx.animation.KeyFrame;
+
 import javafx.animation.PauseTransition;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Popup;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class VentanaEmergente {
-
+    
+    //UNICO METODO ESTATICO QUE INSTANCIA UNA VENTANA EMERGENTE EN EL JUEGO, CON UN MENSAJE ESPECIFICO Y UN TIEMPO DETERMINADO
     public static HBox aparecer(String mensaje, int duracion) {
-        HBox ventana = new HBox();
-        ventana.setStyle("-fx-background-color: rgba(100, 100, 100, 0.7); -fx-background-radius: 10;");
+        HBox ventana = new HBox();//BOX DEL MENSAJE
+        ventana.setStyle("-fx-background-color: rgba(100, 100, 100, 0.7); -fx-background-radius: 10;");//ESTILO
         
-        Label label = new Label(mensaje);
-        label.setStyle("-fx-font-size: 20; -fx-text-fill: white;");
+        Label label = new Label(mensaje);//TEXTO DEL MENSAJE
+        label.setStyle("-fx-font-size: 20; -fx-text-fill: white;");//ESTILO
         
+        //GUARDA Y MUESTRA LA VENTANA EMERGENTE
         ventana.getChildren().addAll(label);      
         ventana.setVisible(true);
         
+        //DEFINE EL TIEMPO EN EL QUE APARECE EN EL JUEGO LA VENTANA EMERGENTE
         PauseTransition delay = new PauseTransition(Duration.seconds(duracion));
             delay.setOnFinished(e -> ventana.setVisible(false));
-            delay.play();
-             
-        return ventana;
-    
-    }
-
-    
+            delay.play();            
+        return ventana;   
+    }   
 }
 
